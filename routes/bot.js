e0a6b9847ed60ec33651ba35d60f8f43ce707229
ahnsy <ahnsy29@gmail.com>
2018-02-04 16:45:31 +0900

    api test

--- routes/bot.js
+++ routes/bot.js
@@ -12,4 +12,29 @@ router.get('/keyboard', (req, res) => {
     }).send(JSON.stringify(menu))
 });
 
+router.get('/message', (req, res) => {
+    const _obj = {
+        user_key: req.body.user_key,
+        type: req.body.type,
+        content: req.body.content
+    };
+
+    let massage = {
+        "message": {
+            "text": '응답 메세지...'
+        },
+        "keyboard": {
+            "type": "buttons",
+            "buttons": [
+                "메뉴1",
+                "메뉴2",
+                "메뉴3"
+            ]
+        }
+    };
+    res.set({
+        'content-type': 'application/json'
+    }).send(JSON.stringify(massage));
+});
+
 module.exports = router;
