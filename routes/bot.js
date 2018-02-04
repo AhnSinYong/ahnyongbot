ca346c1c60186df180ebdf9911466a7825b9c632
ahnsy <ahnsy29@gmail.com>
2018-02-04 21:28:27 +0900

    api test

--- routes/bot.js
+++ routes/bot.js
@@ -24,7 +24,8 @@ router.post('/message', (req, res) => {
                 axios.get('https://crix-api-endpoint.upbit.com/v1/crix/candles/minutes/1?code=CRIX.UPBIT.KRW-BTC'),
                 axios.get('https://api.bitfinex.com/v2/ticker/tBTCUSD')
             ]).then(axios.spread((upbitKRWRes, bitfinexUSDRes) => {
-                result = message.baseType(`업비트 BTC/KRW는 ${upbitKRWRes.data[0].tradePrice}입니다. 비트파이넥스 BTC/USD는 ${bitfinexUSDRes.data[6]}입니다.`);
+                result = message.baseType(`업비트 BTC/KRW [${upbitKRWRes.data[0].tradePrice}]
+                비트파이넥스 BTC/USD [${bitfinexUSDRes.data[6]}]`);
                 res.set({
                     'content-type': 'application/json'
                 }).send(JSON.stringify(result));
@@ -36,7 +37,9 @@ router.post('/message', (req, res) => {
                 axios.get('https://api.binance.com/api/v3/ticker/price?symbol=TRXETH'),
                 axios.get('https://api.coinnest.co.kr/api/pub/ticker?coin=tron')
             ]).then(axios.spread((binanceBTCRes, binanceETHRes, coinnestKRWRes) => {
-                result = message.baseType(`바이낸스 TRX/BTC는 ${binanceBTCRes.data.price}입니다. 바이낸스 TRX/ETH는 ${binanceETHRes.data.price}입니다. 코인네스트 TRX/KRW는 ${coinnestKRWRes.data.last}입니다.`);
+                result = message.baseType(`바이낸스 TRX/BTC [${binanceBTCRes.data.price}]
+                바이낸스 TRX/ETH [${binanceETHRes.data.price}]
+                코인네스트 TRX/KRW [${coinnestKRWRes.data.last}]`);
                 res.set({
                     'content-type': 'application/json'
                 }).send(JSON.stringify(result));
