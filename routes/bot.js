093eb1d993a9804ed38080f5aa9cec9fcf3aa8f1
ahnsy <ahnsy29@gmail.com>
2018-02-04 16:58:16 +0900

    api test

--- routes/bot.js
+++ routes/bot.js
@@ -1,24 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/keyboard', (req, res) => {
-    const menu = {
-        type: 'buttons',
-        buttons: ["비트코인", "트론"]
-    };
+const message = require('./message');
 
+router.get('/keyboard', (req, res) => {
     res.set({
         'content-type' : 'application/json'
-    }).send(JSON.stringify(menu))
+    }).send(JSON.stringify(message.buttonsType()))
 });
 
-router.get('/message', (req, res) => {
+router.post('/message', (req, res) => {
     const _obj = {
         user_key: req.body.user_key,
         type: req.body.type,
         content: req.body.content
     };
-
     let massage = {
         "message": {
             "text": '응답 메세지...'
