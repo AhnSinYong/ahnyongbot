1e3918d78262d492c3f62931662674671e114f7b
ahnsy <ahnsy29@gmail.com>
2018-02-04 20:06:59 +0900

    api test

--- routes/bot.js
+++ routes/bot.js
@@ -24,10 +24,8 @@ router.post('/message', (req, res) => {
                 axios.get('https://crix-api-endpoint.upbit.com/v1/crix/candles/minutes/1?code=CRIX.UPBIT.KRW-BTC'),
                 axios.get('https://api.bitfinex.com/v2/ticker/tBTCUSD')
             ]).then(axios.spread((upbitKRWRes, bitfinexUSDRes) => {
-                console.log(upbitKRWRes);
-                console.log(bitfinexUSDRes);
-                result = message.baseType("업비트 BTC/KRW는" + upbitKRWRes.tradePrice + "입니다.\n" +
-                    "비트파이넥스 BTC/USD는" + bitfinexUSDRes[6] + "입니다.");
+                result = message.baseType("업비트 BTC/KRW는" + upbitKRWRes.data[0].tradePrice + "입니다.\n" +
+                    "비트파이넥스 BTC/USD는" + bitfinexUSDRes.data[6] + "입니다.");
             }));
             break;
         case message.buttons[1]:
@@ -36,12 +34,9 @@ router.post('/message', (req, res) => {
                 axios.get('https://api.binance.com/api/v3/ticker/price?symbol=TRXETH'),
                 axios.get('https://api.coinnest.co.kr/api/pub/ticker?coin=tron')
             ]).then(axios.spread((binanceBTCRes, binanceETHRes, coinnestKRWRes) => {
-                console.log(binanceBTCRes);
-                console.log(binanceETHRes);
-                console.log(coinnestKRWRes);
                 result = message.baseType("바이낸스 TRX/BTC는" + binanceBTCRes.price + "입니다.\n" +
-                    "바이낸스 TRX/ETH는" + binanceETHRes.price + "입니다.\n" +
-                    "코인네스트 TRX/KRW는" + coinnestKRWRes.last + "입니다.");
+                    "바이낸스 TRX/ETH는" + binanceETHRes.data.price + "입니다.\n" +
+                    "코인네스트 TRX/KRW는" + coinnestKRWRes.data.last + "입니다.");
             }));
             break;
         default:
